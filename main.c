9b22b9290a9c466d5611d1ec1773831fc29c909c
Samuel Michalik <samuelmichalik@yahoo.com>
2019-01-10 19:02:53 +0100

    refactoring main a bit

--- main.c
+++ main.c
@@ -29,6 +29,10 @@ void sig_handler(int sig);
 
 int readline_line_reset(void);
 
+int interactive_mode(void);
+
+void execute_script(char *scrpath);
+
 int
 main(int argc, char **argv)
 {
@@ -70,128 +74,16 @@ main(int argc, char **argv)
 	}
 
 	if (argv[optind] == NULL) {
-		/*
-		 * Interactive mode
-		 */
+		/* interactive mode */
+		int rv = interactive_mode();
+		return (rv);
 
-		sigset_t sigs, osigs;
-		sigemptyset(&sigs);
-		sigaddset(&sigs, SIGINT);
-		sigprocmask(SIG_SETMASK, &sigs, &osigs);
-
-		char *line = (char *)NULL;
-
-		do {
-			if (line != NULL) {
-				free(line);
-			}
-
-			char *prompt = get_prompt();
-			sigprocmask(SIG_UNBLOCK, &sigs, NULL);
-			line = readline(prompt);
-			sigprocmask(SIG_SETMASK, &sigs, NULL);
-			free(prompt);
-
-			/* parse the line and execute the commands sequentially */
-			if (line != NULL) {
-				add_history(line);
-
-				/*
-				 * Readline's stripping of the newline character complicates
-				 * bison's parsing. Rather than changing the grammar we just
-				 * append the newline artificially.
-				 */
-				char *nl = str_cat(line, "\n");
-				free(line);
-				line = nl;
-
-				yy_scan_string(line);
-				yyparse();
-			}
-		} while (line != NULL);
-
-		printf("\n");
-		sigprocmask(SIG_UNBLOCK, &osigs, NULL);
-
-		return (ret_val);
-		
 	} else {
-		/*
-		 * Non-interactive mode
-		 */
-
+		/* non-interactive mode */
 		argv = &argv[optind];
 
-		char *fpath;
 		while (*argv != NULL) {
-			fpath = realpath(*argv, NULL);
-
-			/*
-			 * Reading input from files could be simplified
-			 * to a few lines if fopen was allowed.
-			 */
-			int fd = open(fpath, O_RDONLY);
-			if (fd == -1) {
-				err(1, "open");
-			} else {
-				char buf[BUFFSIZE];
-				char *line = (char *)NULL;
-				int r;
-
-				lineno = 0;
-				while ((r = read(fd, buf, BUFFSIZE)) != -1) {
-					int pos = 0;
-					int old_pos = -1;
-
-					while (pos < r) {
-						/* on newline encounter execute the built line */
-						if (buf[pos] == 10) {
-							int len = (pos - old_pos);
-							char *sub_line = malloc((len + 1) * sizeof (char));
-
-							for (int i = 0; i < len; i++) {
-								*(sub_line + i) = buf[old_pos + i + 1];
-							}
-							*(sub_line + len) = '\0';
-							if (line == NULL) {
-								line = sub_line;
-							} else {
-								char *new_line = str_cat(line, sub_line);
-								free(line);
-								free(sub_line);
-								line = new_line;
-							}
-
-							lineno++;
-							yy_scan_string(line);
-							yyparse();
-							free(line);
-							line = (char *)NULL;
-							old_pos = pos;
-
-							/* encountered a syntax error */
-							if (ret_val == 254) {
-								exit(ret_val);
-							}
-						}
-
-						pos++;
-					}
-
-					if (r == 0) {
-						break;
-					}
-				}
-
-				if (r == -1) {
-					err(1, "read");
-				}
-			}
-
-			if (close(fd) == -1) {
-				err(1, "close");
-			}
-			free(fpath);
+			execute_script(*argv);
 			argv++;
 		}
 	}
@@ -213,4 +105,120 @@ readline_line_reset(void)
 	rl_redisplay();
 
 	return (0);
+}
+
+int
+interactive_mode(void)
+{
+	sigset_t sigs, osigs;
+	sigemptyset(&sigs);
+	sigaddset(&sigs, SIGINT);
+	sigprocmask(SIG_SETMASK, &sigs, &osigs);
+
+	char *line = (char *)NULL;
+
+	/* read lines from user input */
+	do {
+		if (line != NULL) {
+			free(line);
+		}
+
+		char *prompt = get_prompt();
+		sigprocmask(SIG_UNBLOCK, &sigs, NULL);
+		line = readline(prompt);
+		sigprocmask(SIG_SETMASK, &sigs, NULL);
+		free(prompt);
+
+		/* parse the line and execute the commands sequentially */
+		if (line != NULL) {
+			add_history(line);
+
+			/*
+			 * Readline's stripping of the newline character complicates
+			 * bison's parsing. Rather than changing the grammar we just
+			 * append the newline artificially.
+			 */
+			char *nl = str_cat(line, "\n");
+			free(line);
+			line = nl;
+
+			yy_scan_string(line);
+			yyparse();
+		}
+	} while (line != NULL);
+
+	printf("\n");
+	sigprocmask(SIG_UNBLOCK, &osigs, NULL);
+
+	return (ret_val);		
+}
+
+void
+execute_script(char *scrpath)
+{
+	char *fpath = realpath(scrpath, NULL);
+
+	int fd = open(fpath, O_RDONLY);
+	if (fd == -1) {
+		err(1, "open");
+	} else {
+		char buf[BUFFSIZE];
+		char *line = (char *)NULL;
+		int r;
+
+		lineno = 0;
+		while ((r = read(fd, buf, BUFFSIZE)) != -1) {
+			int pos = 0;
+			int old_pos = -1;
+
+			while (pos < r) {
+				/* on newline encounter execute the built line */
+				if (buf[pos] == 10) {
+					int len = (pos - old_pos);
+					char *sub_line = malloc((len + 1) * sizeof (char));
+
+					for (int i = 0; i < len; i++) {
+						*(sub_line + i) = buf[old_pos + i + 1];
+					}
+					*(sub_line + len) = '\0';
+					if (line == NULL) {
+						line = sub_line;
+					} else {
+						char *new_line = str_cat(line, sub_line);
+						free(line);
+						free(sub_line);
+						line = new_line;
+					}
+
+					lineno++;
+					yy_scan_string(line);
+					yyparse();
+					free(line);
+					line = (char *)NULL;
+					old_pos = pos;
+
+					/* encountered a syntax error */
+					if (ret_val == 254) {
+						exit(ret_val);
+					}
+				}
+
+				pos++;
+			}
+
+			if (r == 0) {
+				printf("Exiting this way\n");
+				break;
+			}
+		}
+
+		if (r == -1) {
+			err(1, "read");
+		}
+	}
+
+	if (close(fd) == -1) {
+		err(1, "close");
+	}
+	free(fpath);
 }

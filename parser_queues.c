b40b2f994e1e94bbad4c8237db1307dbb65a143f
Samuel Michalik <samuelmichalik@yahoo.com>
2019-01-11 19:36:50 +0100

    remove unused includes

--- parser_queues.c
+++ parser_queues.c
@@ -1,11 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <err.h>
-#include <signal.h>
 
 #include "parser_queues.h"
 #include "mysh.h"
